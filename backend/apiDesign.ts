767c1112ff6f697f8415c98527b7081097200fef
buihoanganhtuan <buihoanganhtuank56@gmail.com>
2023-04-04 19:43:51 +0900

    api definitions update

--- backend/apiDesign.ts
+++ backend/apiDesign.ts
@@ -2,45 +2,96 @@ abstract class TripPlannerApi {
     static version = "v1";
     static title = "Trip Planner API"
 
-    // resource: User
+    // resource type: User
     @post("/users")
     CreateUser(req: CreateUserRequest) : User;
 
-    @get("/{name=users/*}")
+    @get("/{id=users/*}")
     GetUser(req: GetUserRequest) : User;
 
     @get("/users")
     ListUsers(req: ListUsersRequest) : User[];
 
-    @patch("/{name=users/*}")
+    @patch("/{resource.name=users/*}")
     UpdateUser(req: UpdateUserRequest) : User;
 
-    @delete("/{name=/users/*}")
+    @put("/{resource.name=users/*}")
+    ReplaceUser(req: ReplaceUserRequest) : User;
+
+    @delete("/{id=users/*}")
     DeleteUser(req: DeleteUserRequest) : void;
+
+    // resource type: User.Trip
+    @post("/{resource.userId=users/*}/trips")
+    CreateTrip(req: CreateTripRequest) : Trip;
+
+    @get("/{id=users/*/trips/*}")
+    GetTrip(req: GetTripRequest) : Trip;
+
+    @patch("/{resource.id=users/*/trips/*}")
+    UpdateTrip(req: UpdateTripRequest) : Trip;
+
+    @put("/{resource.id=users/*/trips/*}")
+    ReplaceTrip(req: ReplaceTripRequest) : Trip;
+
+    @get("{parent=users/*}/trips")
+    ListTrips(req: ListTripRequest) : Trip[];
+
+    @delete("/{id=users/*/trips/*}")
+    DeleteTrip(req: DeleteTripRequest) : void;
 
 }
 
 interface CreateUserRequest {
-    user: User;
+    resource: User;
 }
 
-// Just to be user friendly. Technically, GetUserRequest should have contained id instead
+// fieldMask shall be sent as query string to avoid violating constraint on standard update method
+interface UpdateUserRequest {
+    resource: User;
+    fieldMask: string[];
+}
+
+interface ReplaceUserRequest {
+    resource: User;
+}
+
 interface GetUserRequest {
-    name: string;
+    id: string;
 }
 
 interface ListUsersRequest {
     filter: string;
 }
 
-// fieldMask shall be sent as query string
-interface UpdateUserRequest {
-    user: User;
+interface DeleteUserRequest {
+    name: string;
+}
+
+interface CreateTripRequest {
+    resource: Trip;
+}
+
+interface UpdateTripRequest {
+    resource: Trip;
     fieldMask: string[];
 }
 
-interface DeleteUserRequest {
-    name: string;
+interface ReplaceTripRequest {
+    resource: Trip;
+}
+
+interface GetTripRequest {
+    id: string;
+}
+
+interface ListTripRequest {
+    parent: string;
+    filter: string;
+}
+
+interface DeleteTripRequest {
+    id: string;
 }
 
 interface User {
@@ -49,5 +100,27 @@ interface User {
     name: string;
     password: string;
     email: string;
-    joinDate: string;
+    joinDate: Datetime;
+}
+
+// Polymorphic resource, can be either trip planned by an anonymous user or a registered user
+interface Trip {
+    id: string;
+    type: "anonymous" | "registered";
+
+    userId?: string;
+    name?: string;
+    dateExpected: Datetime;
+    dateCreated: Datetime;
+}
+
+// Data types
+interface Datetime {
+    year: number;
+    month: number;
+    day: number;
+    hour: number;
+    min: number;
+    sec: number;
+    timezone: string;
 }
