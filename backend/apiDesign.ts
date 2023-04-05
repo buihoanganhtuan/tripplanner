1817146fdb88b1a7946209cff1581805c52f1479
buihoanganhtuan <buihoanganhtuank56@gmail.com>
2023-04-05 19:57:13 +0900

    api definition update

--- backend/apiDesign.ts
+++ backend/apiDesign.ts
@@ -22,7 +22,7 @@ abstract class TripPlannerApi {
     DeleteUser(req: DeleteUserRequest) : void;
 
     // resource type: Trip
-    @post("/{resource.userId=users/*}/trips")
+    @post("/{parent=users/*}/trips" | "/trips")
     CreateTrip(req: CreateTripRequest) : Trip;
 
     @get("/{id=users/*/trips/*}")
@@ -40,8 +40,33 @@ abstract class TripPlannerApi {
     @delete("/{id=users/*/trips/*}")
     DeleteTrip(req: DeleteTripRequest) : void;
 
+    @post("/{id=users/*/trips/*}:plan" | "/{id=trips/*}:plan")
+    PlanTrip(req: PlanTripRequest) : Operation<ResultT, MetadataT>; // custom method for planning the trip
+
+    // Resource type: Point
+    @post("/{parent=users/*/trips/*}/points" | "/{parent=trips/*}/points")
+    CreatePoint(req: CreatePointRequest) : Point;
+
+    @patch("/{resource.id=users/*/trips/*/points/*}" | "/{resource.id=trips/*/points/*}")
+    UpdatePoint(req: UpdatePointRequest): Point;
+
+    @put("/{resource.id=users/*/trips/*/points/*}" | "/{resource.id=trips/*/points/*}")
+    ReplacePoint(req: ReplacePointRequest) : Point;
+
+    @get("/{id=users/*/trips/*/points/*}" | "/{id=trips/*/points/*}")
+    GetPoint(req: GetPointRequest) : Point;
+
+    @get("/{parent=users/*/trips/*}/points" | "/{parent=trips/*}/points")
+    ListPoints(req: ListPointRequest) : Point[];
+
+    @delete("/{id=users/*/trips/*/points/*}" | "/{id=trips/*/points/*}")
+    DeletePoint(req: DeletePointRequest) : void;
+
+    @post("/{id=users/*/trips/*/points/*}:copy | {id=trips/*/points/*}:copy") // custom method for copy a point from one trip to another. Won't copy pointConstraints
+    CopyPoint(req: CopyPointRequest) : Point;
+
     // singleton subresource: UserPassword
-    @patch("/{resource.id=users/*/password}")
+    @post("/{resource.id=users/*/password}")
     UpdateUserPassword(req: UpdateUserPasswordRequest) : UserPassword;
 
     @post("/{id=users/*/password}:reset")
@@ -100,6 +125,10 @@ interface DeleteTripRequest {
     id: string;
 }
 
+interface PlanTripRequest {
+    id: string;
+}
+
 interface ResetUserPasswordRequest {
     id: string;
 
@@ -114,6 +143,39 @@ interface UpdateUserPasswordRequest {
     resource: UserPassword;
 }
 
+interface CreatePointRequest {
+    parent: string;
+    resource: Point;
+}
+
+interface UpdatePointRequest {
+    resource: Point;
+}
+
+interface ReplacePointRequest {
+    resource: Point;
+}
+
+interface GetPointRequest {
+    id: string;
+}
+
+interface ListPointRequest {
+    parent: string;
+    filter: string;
+}
+
+interface DeletePointRequest {
+    id: string;
+}
+
+interface CopyPointRequest {
+    id: string;
+    destinationTripId: string;
+    destinationId: string;
+}
+
+// ********************* Resource type definitions ************************
 interface User {
     id: string;
 
@@ -126,8 +188,7 @@ interface User {
 interface UserPassword {
     id: string;
 
-    oldPassword: string;
-    password: string;
+    value: string;
 }
 
 // Polymorphic resource, can be either trip planned by an anonymous user or a registered user
@@ -139,6 +200,7 @@ interface Trip {
     name?: string;
     dateExpected: Datetime;
     dateCreated: Datetime;
+    lastModified: Datetime;
     budgetLimit: Cost;
     preferredTransportMode: 'train' | 'bus' | 'walk';
 }
@@ -150,6 +212,20 @@ interface Point {
     geoPointId: string;
     constraints: PointConstraints;
     priority?: number;
+}
+
+interface Operation<ResultT, MetadataT> {
+    id: string;
+
+    done: Boolean;
+    result: ResultT | OperationError
+    metadata: MetadataT;
+}
+
+interface OperationError {
+    code: string;
+    messsage: string;
+    details?: string;
 }
 
 // Data types
