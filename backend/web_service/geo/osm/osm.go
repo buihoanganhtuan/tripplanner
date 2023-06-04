d938bb442092a44a8d716ee53e3a53608f7900e1
buihoanganhtuan <buihoanganhtuank56@gmail.com>
2023-06-05 02:37:42 +0900

    wip: osm data handling

--- backend/web_service/geo/osm/osm.go
+++ backend/web_service/geo/osm/osm.go
@@ -2,164 +2,266 @@ package osm
 
 import (
 	"database/sql"
+	"math"
 	"strconv"
 	"strings"
 
+	"github.com/buihoanganhtuan/tripplanner/backend/web_service/datastructure"
 	"github.com/buihoanganhtuan/tripplanner/backend/web_service/domain"
-	"github.com/buihoanganhtuan/tripplanner/backend/web_service/geo"
+	"github.com/paulmach/osm"
 )
 
-type Osm struct {
-	Dom *domain.Domain
-	Db  *sql.DB
-
-	GeohashLen int
+type OsmGeo struct {
+	Domain domain.Domain
+	Db     *sql.DB
 }
 
-type hashId int64
+const (
+	latBits = 20
+	lonBits = 21
+)
 
-func (o *Osm) findTransitNodes(gp domain.GeoPoint, dist float64) ([]domain.GeoPoint, error) {
-	lat := gp.Lat
-	lon := gp.Lon
+// Contract implementation
 
-	latBits := o.GeohashLen / 2
-	lonBits := o.GeohashLen/2 + o.GeohashLen%2
-	numLats := int64(1) << int64(latBits)
-	numLons := int64(1) << int64(lonBits)
-	dlat := float64(180) / float64(numLats)
-	dlon := float64(360) / float64(numLons)
-
-	var lo, hi int
-	hi = int(lat / dlat)
-	for lo < hi {
-		mid := (lo + hi + 1) / 2
-		mlat := float64(-90) + float64(mid)*dlat
-		if geo.Haversine(lat, lon, mlat, lon) >= dist {
-			lo = mid
-		} else {
-			hi = mid - 1
-		}
+func (o *OsmGeo) GeoPoint(id domain.GeoPointId) (domain.GeoPoint, error) {
+	row := o.Db.QueryRow(`SELECT id, lat, lon, level, name FROM  WHERE id = ?`, id)
+	var p domain.GeoPoint
+	if err := row.Scan(&p.Id, &p.Lat, &p.Lon, &p.Level, &p.Name); err != nil {
+		return p, err
 	}
-	blat := int64(lo)
+	return p, nil
+}
 
-	lo = int(lat/dlat) + 1
-	hi = 1<<latBits - 1
-	for lo < hi {
-		mid := (lo + hi) / 2
-		mlat := float64(-90) + float64(mid)*dlat
-		if geo.Haversine(lat, lon, mlat, lon) >= dist {
-			hi = mid
-		} else {
-			lo = mid + 1
-		}
+func (o *OsmGeo) GeoPoints(ids []domain.GeoPointId) ([]domain.GeoPoint, error) {
+	var ss []string
+	for _, id := range ids {
+		ss = append(ss, string(id))
 	}
-	tlat := int64(lo)
-
-	lo = 0
-	hi = int(lon / dlon)
-	for lo < hi {
-		mid := (lo + hi + 1) / 2
-		mlon := float64(-180) + float64(mid)*dlon
-		if geo.Haversine(lat, lon, lat, mlon) >= dist {
-			lo = mid
-		} else {
-			hi = mid - 1
-		}
-	}
-	llon := int64(lo)
-
-	lo = int(lon/dlon) + 1
-	hi = 1<<lonBits - 1
-	for lo < hi {
-		mid := (lo + hi) / 2
-		mlon := float64(-180) + float64(mid)*dlon
-		if geo.Haversine(lat, lon, lat, mlon) >= dist {
-			hi = mid
-		} else {
-			lo = mid + 1
-		}
-	}
-	rlon := int64(lo)
-
-	var geoHashes []hashId
-	for j := blat; j <= tlat; j++ {
-		for k := llon; k <= rlon; k++ {
-			geoHashes = append(geoHashes, hashId(j+k<<latBits))
-		}
-	}
-
-	gpp, err := o.geoPointsWithHashes(geoHashes)
+	list, err := parenthesize(ss, '(', ')', ',')
 	if err != nil {
 		return nil, err
 	}
-
-	var tmp []domain.GeoPoint
-	for _, gp := range gpp {
-		if geo.Haversine(lat, lon, gp.Lat, gp.Lon) > dist {
-			continue
-		}
-		tmp = append(tmp, gp)
-	}
-
-	return tmp, nil
-}
-
-func (o *Osm) geoPointsWithHashes(hh []hashId) ([]domain.GeoPoint, error) {
-	var hs []string
-	for _, h := range hh {
-		hs = append(hs, strconv.FormatInt(int64(h), 10))
-	}
-	q := `SELECT Id, Lat, Lon, Name, Address, Tags FROM  WHERE HashId in (?)`
-	rows, err := o.Db.Query(q, strings.Join(hs, ","))
-	if err != nil {
-		return nil, err
-	}
-
-	var res []domain.GeoPoint
+	rows, err := o.Db.Query(`SELECT id, lat, lon, level, name FROM  WHERE id IN ?`, list)
+	var ret []domain.GeoPoint
 	for rows.Next() {
-		var id, name, addr, tags string
-		var lat, lon float64
+		var p domain.GeoPoint
+		if err = rows.Scan(&p.Id, &p.Lat, &p.Lon, &p.Level, &p.Name); err != nil {
+			return nil, err
+		}
+		ret = append(ret, p)
+	}
+	return ret, nil
+}
 
-		var tokens []string
-		tokens = strings.Split(addr, " ")
-		var add domain.Address
-		if len(tokens) > 0 {
-			add.Prefecture = tokens[0]
+func (o *OsmGeo) EdgesTo(id domain.GeoPointId, transport string) ([]domain.GeoEdge, error) {
+	rows, err := o.Db.Query(`SELECT id, from, to, edgesCount, travelTime, leftChild, rightChild, `+transport+` WHERE to = ?`, id)
+	if err != nil {
+		return nil, err
+	}
+	var ret []domain.GeoEdge
+	for rows.Next() {
+		var e domain.GeoEdge
+		var ttfStr string
+		var ttf []domain.TravelTime
+		if err = rows.Scan(&e.Id, &e.From, &e.To, &e.OriginalEdges, &ttfStr, e.LeftChild, e.RightChild); err != nil {
+			return nil, err
 		}
-		if len(tokens) > 1 {
-			add.City = tokens[1]
+		if ttf, err = parseTTF(ttfStr); err != nil {
+			return nil, err
 		}
-		if len(tokens) > 2 {
-			add.District = tokens[2]
-		}
-		if len(tokens) > 3 {
-			add.LandNumber = tokens[3]
-		}
+		e.TravelTimeFunction = ttf
+	}
+}
 
-		// handletags
-		var t []domain.KeyValuePair
-		for _, kv := range strings.Split(tags, ";") {
-			tokens = strings.Split(kv, ":")
-			if len(tokens) != 2 {
-				continue
-			}
-			t = append(t, domain.KeyValuePair{
-				Key:   tokens[0],
-				Value: tokens[1],
-			})
+// routines
+var (
+	walkable   = datastructure.NewDefaultSet[string]("highway=footway", "")
+	unwalkable = datastructure.NewDefaultSet[string]()
+)
+
+func (o *OsmGeo) handleOsmNode(node osm.Node) {
+
+}
+
+func extractWalkWay(way osm.Way) string {
+
+}
+
+func extractBusRoute(rel osm.Relation) string {
+	mems := []osm.Member(rel.Members)
+
+}
+
+func extractTrainRoute(rel osm.Relation) string {
+
+}
+
+// sometimes a train stop node is not connected to any way in/out. This function
+// aims to remedy that by binding it to the nearest platform (way), which is supposedly
+// connected to the outside world
+func (o *OsmGeo) findNearestPlatform(n *osm.Node) (int, error) {
+	dlat := 180. / float64(1<<latBits)
+	dlon := 360. / float64(1<<lonBits)
+	var i, j int64 = int64(n.Lat / dlat), int64(n.Lon / dlon)
+	var hh []string
+	for di := int64(-1); di <= 1; di++ {
+		for dj := int64(-1); dj <= 1; dj++ {
+			hh = append(hh, strconv.FormatInt((i+di)+(j+dj)<<latBits, 10))
 		}
-		res = append(res, domain.GeoPoint{
-			Id:      domain.GeoPointId(id),
-			Lat:     lat,
-			Lon:     lon,
-			Name:    &name,
-			Address: add,
-			Tags:    t,
+	}
+	rows, err := o.Db.Query(`
+	SELECT 
+	FROM (SELECT id FROM  WHERE type = platform AND hash IN (?)) t1
+	JOIN (SELECT id, from, to FROM  WHERE type = platform) t2
+	ON t1.id = t2.from OR t1.id = t2.to`, strings.Join(hh, ","))
+	if err != nil {
+		return 0, err
+	}
+
+	for rows.Next() {
+
+	}
+}
+
+// helper functions
+func isWalkableWay(w *osm.Way) bool {
+	tags := w.TagMap()
+	// https://taginfo.openstreetmap.org/keys/footway#values
+	if fw, ok := tags["footway"]; ok && (fw == "sidewalk" || fw == "crossing" || fw == "access_aisle") {
+		return true
+	}
+	hw, ok := tags["highway"]
+	if !ok {
+		return false
+	}
+	if tv, ok := tags["trail_visibility"]; ok {
+		return tv == "excellent" || tv == "good" || tv == "intermediate"
+	}
+	if ss, ok := tags["sac_scale"]; ok {
+		return ss == "hiking" || ss == "mountain_hiking" || ss == "demanding_mountain_hiking"
+	}
+	if ft, ok := tags["foot"]; ok {
+		return ft == "yes" || ft == "designated" || ft == "permissive"
+	}
+	if pt, ok := tags["public_transport"]; ok {
+		return pt == "platform"
+	}
+	// https://taginfo.openstreetmap.org/keys/highway#values
+	if hw == "footway" || hw == "residential" || hw == "pedestrian" || hw == "track" || hw == "unclassified" || hw == "path" || hw == "platform" || hw == "corridor" || hw == "steps" || hw == "elevator" {
+		return true
+	}
+	return false
+}
+
+func isBusRoute(r *osm.Relation) bool {
+	tags := r.TagMap()
+	if tp, ok := tags["type"]; !ok || tp != "route" {
+		return false
+	}
+	if rt, ok := tags["route"]; !ok || rt != "bus" && rt != "trolleybus" {
+		return false
+	}
+	// ver1 has problems with bus stop ordering
+	if ver, ok := tags["public_transport:version"]; !ok || ver != "2" {
+		return false
+	}
+	return true
+}
+
+func isTrainRoute(r *osm.Relation) bool {
+	tags := r.TagMap()
+	if tp, ok := tags["type"]; !ok || tp != "route" {
+		return false
+	}
+	if rt, ok := tags["route"]; !ok || rt != "train" {
+		return false
+	}
+	if ver, ok := tags["public_transport:version"]; !ok || ver != "2" {
+		return false
+	}
+	return true
+}
+
+func haversine(lat1, lon1, lat2, lon2 float64) float64 {
+	r := 6378.137e3
+	a := math.Sin((lat2 - lat1) / 2)
+	b := math.Sin((lon2 - lon1) / 2)
+	return 2 * r * math.Asin(math.Sqrt(a*a+math.Cos(lat1)*math.Cos(lat2)*b*b))
+}
+
+func parseTTF(ttf string) ([]domain.TravelTime, error) {
+	ss := strings.Split(ttf, ";")
+	var ret []domain.TravelTime
+	for _, s := range ss {
+		tmp := strings.Split(s, ":")
+		time, err := strconv.ParseFloat(tmp[0], 64)
+		if err != nil {
+			return nil, err
+		}
+		value, err := strconv.ParseFloat(tmp[1], 64)
+		if err != nil {
+			return nil, err
+		}
+		ret = append(ret, domain.TravelTime{
+			At:    time,
+			Value: value,
 		})
 	}
+	return ret, nil
+}
 
-	if rows.Err() != nil {
-		return nil, rows.Err()
+func parenthesize(ss []string, left rune, right rune, sep rune) (string, error) {
+	var sb strings.Builder
+	if _, err := sb.WriteRune(left); err != nil {
+		return "", err
 	}
-	return res, nil
+	for i, s := range ss {
+		if _, err := sb.WriteString(s); err != nil {
+			return "", err
+		}
+		if i == len(ss)-1 {
+			continue
+		}
+		if _, err := sb.WriteRune(sep); err != nil {
+			return "", err
+		}
+	}
+	if _, err := sb.WriteRune(right); err != nil {
+		return "", err
+	}
+	return sb.String(), nil
+}
+
+func deg2rad(deg float64) float64 {
+	return deg * math.Pi / 180
+}
+
+func rad2deg(rad float64) float64 {
+	return rad * 180. / math.Pi
+}
+
+func crosstrackDist(lat1, lon1, lat2, lon2, lat3, lon3 float64) float64 {
+	r := 6371.
+	y1 := math.Sin(lon3-lon1) * math.Cos(lat3)
+	x1 := math.Cos(lat1)*math.Sin(lat3) - math.Sin(lat1)*math.Cos(lat3)*math.Cos(lat3-lat1)
+	b1 := math.Atan2(y1, x1)
+	y2 := math.Sin(lon2-lon1) * math.Cos(lat2)
+	x2 := math.Cos(lat1)*math.Sin(lat2) - math.Sin(lat1)*math.Cos(lat2)*math.Cos(lat2-lat1)
+	b2 := math.Atan2(y2, x2)
+	ac := math.Acos(math.Sin(lat1)*math.Sin(lat3) + math.Cos(lat1)*math.Cos(lat3)*math.Cos(lon3-lon1))
+	d := math.Asin(math.Sin(ac)*math.Sin(b1-b2)) * r
+	return math.Abs(d)
+}
+
+func lastLeq[V ~int | ~float64](start int, end int, target V, leqFun func(int, V) bool) int {
+	var l, r int = start, end
+	for l < r {
+		m := (l + r + 1) / 2
+		if leqFun(m, target) {
+			l = m
+		} else {
+			r = m - 1
+		}
+	}
+	return l
 }
--- backend/web_service/go.mod
+++ backend/web_service/go.mod
@@ -7,6 +7,9 @@ require (
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/paulmach/orb v0.1.3 // indirect
+	github.com/paulmach/osm v0.7.1 // indirect
 	github.com/redis/go-redis/v9 v9.0.3 // indirect
+	github.com/twpayne/go-geom v1.5.2 // indirect
 	modernc.org/b/v2 v2.1.0 // indirect
 )
