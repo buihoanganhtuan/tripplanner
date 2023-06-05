6469a7f57ea1e3d4ff3f93897e67b34c8cafc141
buihoanganhtuan <buihoanganhtuank56@gmail.com>
2023-06-05 20:04:12 +0900

    wip: osm

--- backend/web_service/geo/osm/osm.go
+++ backend/web_service/geo/osm/osm.go
@@ -2,11 +2,12 @@ package osm
 
 import (
 	"database/sql"
+	"errors"
+	"fmt"
 	"math"
 	"strconv"
 	"strings"
 
-	"github.com/buihoanganhtuan/tripplanner/backend/web_service/datastructure"
 	"github.com/buihoanganhtuan/tripplanner/backend/web_service/domain"
 	"github.com/paulmach/osm"
 )
@@ -74,53 +75,186 @@ func (o *OsmGeo) EdgesTo(id domain.GeoPointId, transport string) ([]domain.GeoEd
 }
 
 // routines
-var (
-	walkable   = datastructure.NewDefaultSet[string]("highway=footway", "")
-	unwalkable = datastructure.NewDefaultSet[string]()
-)
-
 func (o *OsmGeo) handleOsmNode(node osm.Node) {
 
+}
+
+type stop struct {
+	stopPosition osm.NodeID
+	platform     osm.WayID
+	way          osm.WayID
 }
 
 func extractWalkWay(way osm.Way) string {
 
 }
 
-func extractBusRoute(rel osm.Relation) string {
+func extractRoute(rel osm.Relation) (string, error) {
 	mems := []osm.Member(rel.Members)
+	var stops []stop
+	var i int
+	for ; i < len(mems); i++ {
+		mem := mems[i]
+		if strings.HasPrefix(mem.Role, "stop") {
+			if mem.Type != osm.TypeNode {
+				return "", errors.New(fmt.Sprintf(`relation %v: member %v with role "stop" is not a node`, rel.ID, mem.FeatureID()))
+			}
+			stops = append(stops, stop{
+				stopPosition: osm.NodeID(mem.Ref),
+				platform:     -1,
+				way:          -1,
+			})
+		}
+		if strings.HasPrefix(mem.Role, "platform") {
+			if mem.Type != osm.TypeWay {
+				return "", errors.New(fmt.Sprintf(`relation %v: member %v with role "platform" is not a way`, rel.ID, mem.FeatureID()))
+			}
+			if len(stops) == 0 {
+				return "", errors.New(fmt.Sprintf("relation %v: malformed route format: platform %v is preceded by no stop", rel.ID, mem.FeatureID()))
+			}
+			stops[len(stops)-1].platform = osm.WayID(mem.Ref)
+		}
+		if mem.Role == "" {
+			break
+		}
+	}
 
+	for j := 0; i < len(mems); i++ {
+		mem := mems[i]
+		if mem.Type != osm.TypeWay {
+			return "", errors.New(fmt.Sprintf("relation %v: malformed route format: member %v must be a way on the route, but is not of type way", rel.ID, mem.FeatureID()))
+		}
+		if mem.Role != "" {
+			return "", errors.New(fmt.Sprintf("relation %v: malformed route format: member way %v must have an empty role, but has %s instead", rel.ID, osm.WayID(mem.Ref), mem.Role))
+		}
+		if j >= len(stops) {
+			return "", errors.New(fmt.Sprintf("relation %v: malformed route format: member way %v does not correspond to any stop position", rel.ID, osm.WayID(mem.Ref)))
+		}
+		stops[j].way = osm.WayID(mem.Ref)
+		j++
+	}
+
+	for _, stop := range stops {
+
+	}
 }
 
-func extractTrainRoute(rel osm.Relation) string {
-
-}
-
-// sometimes a train stop node is not connected to any way in/out. This function
+// sometimes a bus stop node is not connected to any way in/out. This function
 // aims to remedy that by binding it to the nearest platform (way), which is supposedly
 // connected to the outside world
-func (o *OsmGeo) findNearestPlatform(n *osm.Node) (int, error) {
+func (o *OsmGeo) nearestBusPlatform(n *osm.Node) (int64, error) {
 	dlat := 180. / float64(1<<latBits)
 	dlon := 360. / float64(1<<lonBits)
 	var i, j int64 = int64(n.Lat / dlat), int64(n.Lon / dlon)
 	var hh []string
-	for di := int64(-1); di <= 1; di++ {
-		for dj := int64(-1); dj <= 1; dj++ {
+	for di := int64(-2); di <= 2; di++ {
+		for dj := int64(-2); dj <= 2; dj++ {
 			hh = append(hh, strconv.FormatInt((i+di)+(j+dj)<<latBits, 10))
 		}
 	}
 	rows, err := o.Db.Query(`
-	SELECT 
-	FROM (SELECT id FROM  WHERE type = platform AND hash IN (?)) t1
-	JOIN (SELECT id, from, to FROM  WHERE type = platform) t2
-	ON t1.id = t2.from OR t1.id = t2.to`, strings.Join(hh, ","))
+	SELECT
+		t3.id AS id, t4.id AS from_id, t4.lat AS from_lat, t4.lon AS from_lon, t5.id AS to_id, t5.lat AS to_lat, t5.lon AS to_lon
+	FROM
+	(
+		SELECT DISTINCT
+			t2.id AS id, from, to
+		FROM 
+			(SELECT id FROM graph.vertices WHERE hash IN (?)) t1
+		INNER JOIN 
+			(SELECT id, from, to FROM graph.edges WHERE type = bus_platform) t2 ON t2.from = t1.id OR t2.to = t1.id
+	) t3
+	INNER JOIN
+		vertices t4 ON t4.id = t3.from
+	INNER JOIN
+		vertices t5 ON t5.id = t3.to
+	`, strings.Join(hh, ","))
 	if err != nil {
 		return 0, err
 	}
 
+	min := math.MaxFloat64
+	var resId int64 = -1
 	for rows.Next() {
-
+		var fromLat, fromLon, toLat, toLon float64
+		var id, fromId, toId int64
+		if err = rows.Scan(&id, &fromId, &fromLat, &fromLon, &toId, &toLat, &toLon); err != nil {
+			return 0, err
+		}
+		d := crosstrackDist(fromLat, fromLon, toLat, toLon, n.Lat, n.Lon)
+		if min <= d {
+			continue
+		}
+		min = d
+		resId = fromId
+		if haversine(n.Lat, n.Lon, fromLat, fromLon) > haversine(n.Lat, n.Lon, toLat, toLon) {
+			resId = toId
+		}
 	}
+	if rows.Err() != nil {
+		return 0, rows.Err()
+	}
+	if resId == -1 {
+		return 0, errors.New(fmt.Sprintf("no nearest platform found for node %v", n.ID))
+	}
+	if min > 50 {
+		return 0, errors.New(fmt.Sprintf("nearest platform for node %s is too far away (node %s at %v meters)", n.ID, resId, int64(min)))
+	}
+	return resId, nil
+}
+
+func (o *OsmGeo) nearestExitGate(n *osm.Node, isSubway bool) (int64, error) {
+	dlat := 180. / float64(1<<latBits)
+	dlon := 360. / float64(1<<lonBits)
+	var i, j int64 = int64(n.Lat / dlat), int64(n.Lon / dlon)
+
+	var hh []string
+	for m := i - 5; m <= i+5; m++ {
+		for n := j - 5; n <= j+5; n++ {
+			hh = append(hh, strconv.FormatInt(i+j<<latBits, 10))
+		}
+	}
+	t := "train_station_entrance"
+	if isSubway {
+		t = "subway_entrance"
+	}
+	rows, err := o.Db.Query(`
+	SELECT
+		id, lat, lon
+	FROM
+		graph.vertices
+	WHERE
+		type = ? AND hash IN (?)
+	`, t, strings.Join(hh, ","))
+	if err != nil {
+		return 0, err
+	}
+
+	min := math.MaxFloat64
+	var resId int64 = -1
+	for rows.Next() {
+		var lat, lon float64
+		var id int64
+		if err = rows.Scan(&id, &lat, &lon); err != nil {
+			return 0, err
+		}
+		d := haversine(lat, lon, n.Lat, n.Lon)
+		if min <= d {
+			continue
+		}
+		min = d
+		resId = id
+	}
+	if rows.Err() != nil {
+		return 0, rows.Err()
+	}
+	if resId == -1 {
+		return 0, errors.New(fmt.Sprintf("no nearest exit found for node %v", n.ID))
+	}
+	if min > 100 {
+		return 0, errors.New(fmt.Sprintf("nearest exit gate for node %s is too far away (node %s at %v meters)", n.ID, resId, int64(min)))
+	}
+	return resId, nil
 }
 
 // helper functions
@@ -173,7 +307,7 @@ func isTrainRoute(r *osm.Relation) bool {
 	if tp, ok := tags["type"]; !ok || tp != "route" {
 		return false
 	}
-	if rt, ok := tags["route"]; !ok || rt != "train" {
+	if rt, ok := tags["route"]; !ok || rt != "train" || rt != "subway" {
 		return false
 	}
 	if ver, ok := tags["public_transport:version"]; !ok || ver != "2" {
@@ -232,12 +366,12 @@ func parenthesize(ss []string, left rune, right rune, sep rune) (string, error) 
 	return sb.String(), nil
 }
 
-func deg2rad(deg float64) float64 {
-	return deg * math.Pi / 180
-}
-
-func rad2deg(rad float64) float64 {
-	return rad * 180. / math.Pi
+func stringify[T any](arr []T, convFun func(T) string) []string {
+	res := make([]string, len(arr))
+	for i := 0; i < len(arr); i++ {
+		res[i] = convFun(arr[i])
+	}
+	return res
 }
 
 func crosstrackDist(lat1, lon1, lat2, lon2, lat3, lon3 float64) float64 {
@@ -253,14 +387,27 @@ func crosstrackDist(lat1, lon1, lat2, lon2, lat3, lon3 float64) float64 {
 	return math.Abs(d)
 }
 
-func lastLeq[V ~int | ~float64](start int, end int, target V, leqFun func(int, V) bool) int {
-	var l, r int = start, end
+func lastLeq(start int64, end int64, target float64, convFun func(int64) float64) int64 {
+	var l, r int64 = start, end
 	for l < r {
 		m := (l + r + 1) / 2
-		if leqFun(m, target) {
+		if convFun(m) <= target {
 			l = m
 		} else {
 			r = m - 1
+		}
+	}
+	return l
+}
+
+func firstLeq(start int64, end int64, target float64, convFun func(int64) float64) int64 {
+	var l, r int64 = start, end
+	for l < r {
+		m := (l + r) / 2
+		if convFun(m) <= target {
+			r = m
+		} else {
+			l = m + 1
 		}
 	}
 	return l
